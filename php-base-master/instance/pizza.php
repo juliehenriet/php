7dd3c9d362ab29f5657fa4db508d8f3e92e28f46
julie <juliehenriet@gmail.com>
2018-10-24 10:58:37 +0200

    requete

--- php-base-master/instance/pizza.php
+++ php-base-master/instance/pizza.php
@@ -7,5 +7,12 @@ $db = new PDO ('mysql:host=localhost;dbname=pizzastore', 'root', '');
   header ('Location : https://www.google.fr/search?q='.$e->getMessage());
 }
 
+//récupérer pizza id 3
+//rfécupérer l'id a partir de l'url
+$id = $_GET ['id'];
+$query = $db->query('SELECT * FROM pizza WHERE id = '.$id);
+$pizza = $query->fetch();
+echo $pizza['id'] .' : '.$pizza['name'];
+
 
 ?>
